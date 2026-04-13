0560e8cfd09546d939e1bdc8068eb937490170d6
Dongya Wu <31070209+dongyawu@users.noreply.github.com>
2026-04-13 16:08:09 +0800

    Update 04a_ont_binning.sh

--- Genome_assembly/assembly/04a_ont_binning.sh
+++ Genome_assembly/assembly/04a_ont_binning.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH --job-name=C081biont
+#SBATCH --job-name=IDbiont
 #SBATCH --partition all
 #SBATCH --nodes=2
 #SBATCH --ntasks=2
@@ -10,15 +10,15 @@
 date
 
 threads=40
-famID="C081-CHA-C01"
+famID="FAMID[should be replaced]"
 
-ont_100k=`ls /slurm/users/wudongya/APG/Nanopore/${famID}-01/*pass_100k.fastq.gz|xargs`
+ont_100k=`ls /path-to-dir/Nanopore/${famID}-01/*pass_100k.fastq.gz|xargs`
 
-pat_ngs=`ls /slurm/users/wudongya/APG/NGS/${famID}-02/*clean.fq.gz|xargs`
-mat_ngs=`ls /slurm/users/wudongya/APG/NGS/${famID}-03/*clean.fq.gz|xargs`
+pat_ngs=`ls /path-to-dir/NGS/${famID}-02/*clean.fq.gz|xargs`
+mat_ngs=`ls /path-to-dir/NGS/${famID}-03/*clean.fq.gz|xargs`
 
 canu -p asm -d binning_100k genomeSize=3g useGrid=true maxThreads=${threads} -haplotypePat $pat_ngs -haplotypeMat $mat_ngs -nanopore-raw $ont_100k -stopAfter=haplotype -corMhapOptions="--threshold 0.8 --ordered-sketch-size 1000 --ordered-kmer-size 14" -correctedErrorRate=0.105
 
-ont=`ls /slurm/users/nas-z/wudongya/APG_raw/ONT/${famID}-01/*.fastq.gz|xargs`
+ont=`ls /path-to-dir/ONT/${famID}-01/*.fastq.gz|xargs`
 
 canu -p asm -d binning genomeSize=3g useGrid=true maxThreads=${threads} -haplotypePat $pat_ngs -haplotypeMat $mat_ngs -nanopore-raw ${ont} -stopAfter=haplotype -corMhapOptions="--threshold 0.8 --ordered-sketch-size 1000 --ordered-kmer-size 14" -correctedErrorRate=0.105
